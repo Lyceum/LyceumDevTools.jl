eee5cdbee94a76c30650be3552ef57aae012ace1
colinxs <me@colinxsummers.com>
2020-04-05 21:07:20 -0700

    update environment tests to new interface

--- src/TestUtil/TestUtil.jl
+++ src/TestUtil/TestUtil.jl
@@ -4,11 +4,13 @@ using ..LyceumDevTools: flattendir
 
 using BenchmarkTools: BenchmarkTools, memory
 using Distributed: Distributed
+using LyceumBase
 using MacroTools: MacroTools
-using Requires: @require
+using Random
+using Shapes
 
-using Test: Test, AbstractTestSet, DefaultTestSet
-using Test: Result, Pass, Fail, Error, Broken
+using Test
+using Test: AbstractTestSet, DefaultTestSet, Result, Pass, Fail, Error, Broken
 
 
 export @qe
@@ -19,9 +21,7 @@ include("macros.jl")
 export ProgressTestSet
 include("progresstestset.jl")
 
-
-function __init__()
-    @require LyceumBase = "db31fed1-ca1e-4084-8a49-12fae1996a55" include("abstractenvironment.jl")
-end
+include("abstractenvironment.jl")
+export testenv_correctness, testenv_inferred, testenv_allocations
 
 end # module
--- src/TestUtil/abstractenvironment.jl
+++ src/TestUtil/abstractenvironment.jl
@@ -1,17 +1,9 @@
-using LyceumBase
-using Test
-using Random
-using Shapes
-
-export testenv_correctness, testenv_inferred, testenv_allocations
-
 function _trajectory(e::AbstractEnvironment, T::Integer)
     (
         states = Array(undef, statespace(e), T),
-        obses = Array(undef, obsspace(e), T),
+        obses = Array(undef, observationspace(e), T),
         acts = Array(undef, actionspace(e), T),
         rews = Array(undef, rewardspace(e), T),
-        evals = Array(undef, evalspace(e), T)
     )
 end
 
@@ -25,10 +17,9 @@ function _rollout(e::AbstractEnvironment, actions::AbstractMatrix)
         ot = view(traj.obses, :, t)
 
         getstate!(st, e)
-        getobs!(ot, e)
+        getobservation!(ot, e)
         setaction!(e, at)
         traj.rews[t] = getreward(st, at, ot, e)
-        traj.evals[t] = geteval(st, at, ot, e)
         step!(e)
     end
     traj
@@ -36,16 +27,16 @@ end
 
 
 function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs...)
-    makeenv() = etype(args...; kwargs...)::AbstractEnvironment
+    makeenv() = etype(args...; kwargs...)
     randstate() = (e = makeenv(); randreset!(e); getstate(e))
-    randobs() = (e = makeenv(); randreset!(e); getobs(e))
+    randobs() = (e = makeenv(); randreset!(e); getobservation(e))
     function randaction()
        e = makeenv()
        a = rand(actionspace(e)) .- 0.5 # random controllers in range [-0.5, 0.5]
        setaction!(e, a)
        # TODO hack for case where env has action limits until BoundedShape exists
        getaction!(a, e)
-       a
+       return a
     end
 
     @testset "Interface" begin
@@ -55,6 +46,7 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
         @testset "time consistency" begin
             e = makeenv()
             @test time(e) isa Float64
+            @test time(e) == 0 # time should always start from 0
             t1 = time(e)
             @test time(e) === t1
             @test step!(e) === e
@@ -62,8 +54,8 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
             @test (t2 - t1) == timestep(e)
         end
 
-        # For each of state/action/observation/reward/eval, test:
-        # 1. return type of getfoo(env) matches foospace(env)
+        # For each of state/action/observation/reward, test:
+        # 1. return type of getfoo(env) is compatible with foospace(env)
         # 2. getfoo!(x, env) returns x
         # 3. getfoo!(x, env) == getfoo(env)
         # 4. if settable, that setfoo!(env, x) returns env
@@ -122,25 +114,25 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
 
         @testset "obs" begin
             let e = makeenv()
-                @test obsspace(e) isa Shapes.AbstractVectorShape
-                @test eltype(getobs(e)) == eltype(obsspace(e))
-                @test eltype(getobs!(rand(obsspace(e)), e)) == eltype(obsspace(e))
-                @test axes(getobs(e)) == axes(obsspace(e))
-                @test axes(getobs!(rand(obsspace(e)), e)) == axes(obsspace(e))
+                @test observationspace(e) isa Shapes.AbstractVectorShape
+                @test eltype(getobservation(e)) == eltype(observationspace(e))
+                @test eltype(getobservation!(rand(observationspace(e)), e)) == eltype(observationspace(e))
+                @test axes(getobservation(e)) == axes(observationspace(e))
+                @test axes(getobservation!(rand(observationspace(e)), e)) == axes(observationspace(e))
             end
 
-            @test let e = makeenv(), x = rand(obsspace(e))
-                x === getobs!(x, e)
+            @test let e = makeenv(), x = rand(observationspace(e))
+                x === getobservation!(x, e)
             end
             @test let e = makeenv()
-                getobs!(rand(obsspace(e)), e) == getobs(e)
+                getobservation!(rand(observationspace(e)), e) == getobservation(e)
             end
         end
 
-        # For getreward, geteval, and isdone, additionally test that they are functions of the
+        # For getreward and isdone, test that they are functions of the
         # passed in (state, action, observation) and not any internal data in the env.
         # NOTE requires that `randreset!` generating a new state with different
-        # reward/eval/isdone
+        # reward/isdone
 
         @testset "reward" begin
             let e = makeenv()
@@ -148,26 +140,11 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
                 @test typeof(getreward(e)) == eltype(rewardspace(e))
             end
 
-            @test let e = makeenv(), s = getstate(e), a = getaction(e), o = getobs(e)
+            @test let e = makeenv(), s = getstate(e), a = getaction(e), o = getobservation(e)
                 r1 = getreward(s, a, o, e)
                 r2 = getreward(e)
                 randreset!(e)
                 r3 = getreward(s, a, o, e)
-                r1 == r2 == r3
-            end
-        end
-
-        @testset "eval" begin
-            let e = makeenv()
-                @test evalspace(e) isa ScalarShape
-                @test typeof(geteval(e)) == eltype(evalspace(e))
-            end
-
-            @test let e = makeenv(), s = getstate(e), a = getaction(e), o = getobs(e)
-                r1 = geteval(s, a, o, e)
-                r2 = geteval(e)
-                randreset!(e)
-                r3 = geteval(s, a, o, e)
                 r1 == r2 == r3
             end
         end
@@ -177,11 +154,11 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
                 isdone(e) isa Bool
             end
 
-            @test let e = makeenv(), s = getstate(e), a = getaction(e), o = getobs(e)
-                d1 = isdone(s, a, o, e)
+            @test let e = makeenv(), s = getstate(e), a = getaction(e), o = getobservation(e)
+                d1 = isdone(s, o, e)
                 d2 = isdone(e)
                 randreset!(e)
-                d3 = isdone(s, a, o, e)
+                d3 = isdone(s, o, e)
                 d1 == d2 == d3
             end
         end
@@ -190,9 +167,8 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
             e1, e2 = makeenv(), makeenv()
             @test getstate(e1) == getstate(e2)
             @test getaction(e1) == getaction(e2)
-            @test getobs(e1) == getobs(e2)
+            @test getobservation(e1) == getobservation(e2)
             @test getreward(e1) == getreward(e2)
-            @test geteval(e1) == geteval(e2)
             @test isdone(e1) == isdone(e2)
         end
 
@@ -214,9 +190,8 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
 
                 @test getstate(e1) == getstate(e2)
                 @test getaction(e1) == getaction(e2)
-                @test getobs(e1) == getobs(e2)
+                @test getobservation(e1) == getobservation(e2)
                 @test getreward(e1) == getreward(e2)
-                @test geteval(e1) == geteval(e2)
                 @test isdone(e1) == isdone(e2)
             end
         end
@@ -234,11 +209,10 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
 
                 @test getstate(e1) != getstate(e2)
                 @test getaction(e1) == getaction(e2)
-                @test getobs(e1) != getobs(e2)
+                @test getobservation(e1) != getobservation(e2)
                 # TODO For some environments, resetting state may not yield
-                # different reward/eval
+                # different reward
                 # @test getreward(e1) != getreward(e2)
-                # @test geteval(e1) != geteval(e2)
             end
 
             let e1 = makeenv(), e2 = makeenv(), rng = Random.MersenneTwister()
@@ -249,11 +223,10 @@ function testenv_correctness(etype::Type{<:AbstractEnvironment}, args...; kwargs
 
                 @test getstate(e1) == getstate(e2)
                 @test getaction(e1) == getaction(e2)
-                @test getobs(e1) == getobs(e2)
+                @test getobservation(e1) == getobservation(e2)
                 # TODO For some environments, resetting state may not yield
-                # different reward/eval
+                # different reward
                 # @test getreward(e1) == getreward(e2)
-                # @test geteval(e1) == geteval(e2)
             end
         end
 
@@ -277,7 +250,7 @@ end
 function testenv_allocations(etype::Type{<:AbstractEnvironment}, args...; kwargs...)
     @testset "Allocations" begin
         e = makeenv()
-        s, a, o = getstate(e), getaction(e), getobs(e)
+        s, a, o = getstate(e), getaction(e), getobservation(e)
 
         @test_noalloc statespace($e)
         @test_noalloc getstate!($s, $e)
@@ -287,19 +260,16 @@ function testenv_allocations(etype::Type{<:AbstractEnvironment}, args...; kwargs
         @test_noalloc getaction!($a, $e)
         @test_noalloc setaction!($e, $a)
 
-        @test_noalloc obsspace($e)
-        @test_noalloc getobs!($o, $e)
+        @test_noalloc observationspace($e)
+        @test_noalloc getobservation!($o, $e)
 
         @test_noalloc rewardspace($e)
         @test_noalloc getreward($s, $a, $o, $e)
 
-        @test_noalloc evalspace($e)
-        @test_noalloc geteval($s, $a, $o, $e)
-
         @test_noalloc reset!($e)
         @test_noalloc randreset!($e)
         @test_noalloc step!($e)
-        @test_noalloc isdone($s, $a, $o, $e)
+        @test_noalloc isdone($s, $o, $e)
         @test_noalloc time($e)
         @test_noalloc timestep($e)
     end
@@ -308,30 +278,27 @@ end
 function testenv_inferred(etype::Type{<:AbstractEnvironment}, args...; kwargs...)
     @testset "Type Stability" begin
         e = makeenv()
-        s, a, o = getstate(e), getaction(e), getobs(e)
+        s, a, o = getstate(e), getaction(e), getobservation(e)
 
-        @inferred statespace(e)
-        @inferred getstate!(s, e)
-        @inferred setstate!(e, s)
+        @test_inferred statespace(e)
+        @test_inferred getstate!(s, e)
+        @test_inferred setstate!(e, s)
 
-        @inferred actionspace(e)
-        @inferred getaction!(a, e)
-        @inferred setaction!(e, a)
+        @test_inferred actionspace(e)
+        @test_inferred getaction!(a, e)
+        @test_inferred setaction!(e, a)
 
-        @inferred obsspace(e)
-        @inferred getobs!(o, e)
+        @test_inferred observationspace(e)
+        @test_inferred getobservation!(o, e)
 
-        @inferred rewardspace(e)
-        @inferred getreward(s, a, o, e)
+        @test_inferred rewardspace(e)
+        @test_inferred getreward(s, a, o, e)
 
-        @inferred evalspace(e)
-        @inferred geteval(s, a, o, e)
-
-        @inferred reset!(e)
-        @inferred randreset!(e)
-        @inferred step!(e)
-        @inferred isdone(s, a, o, e)
-        @inferred time(e)
-        @inferred timestep(e)
+        @test_inferred reset!(e)
+        @test_inferred randreset!(e)
+        @test_inferred step!(e)
+        @test_inferred isdone(s, o, e)
+        @test_inferred time(e)
+        @test_inferred timestep(e)
     end
 end
